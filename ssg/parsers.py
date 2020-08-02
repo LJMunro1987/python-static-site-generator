50ee53531c1dd51f43e53299a03c4ac73fe011f4
Lee Munro <leemunro1987@gmail.com>
2020-08-02 21:10:46 +0100

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -41,4 +41,6 @@ class MarkdownParser(Parser):
 
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
-        html = self.markdown(content.body)
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
